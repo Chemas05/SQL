16a1432c9f1cb4400ffa666d9456bc88e01fcfb6
Chemas05 <jsac_0805@hotmail.com>
2023-08-01 21:45:18 -0500

    Veterinaria

---
+++ Bank.js
@@ -0,0 +1,64 @@
+// const signupform = document.querySelector('#signupform')
+// signupform.addEventListener('submit', (e)=>(
+// e.proventDefault()
+// const username = document.querySelector('username')
+// const password = document.querySelector('password')
+
+// const Users = 
+// ))
+
+
+
+// baseDeDatos = localStorage.getItem("sistema-de-login")
+
+// if (!baseDeDatos) {
+//     cargarDatosIniciales()
+// }
+
+// function cargarDatosIniciales() {
+//     baseDeDatos = {
+//         David: {
+//             contraseña: "12345"
+//         }
+//     sebas: {
+//             contraseña: "67890"
+//         }
+//     Martha: {
+//             contraseña: "10224"
+//         }
+//     }
+// }
+
+// preConfirm: () => {
+//     let usuario = document.getElementById("usuario").value;
+//     let contraseña = document.getElementById("contraseña").value;
+//     if (!usuario) {
+//         Swal.showValidationMessage("No hay usuario")
+//         return false;
+//     }
+//     if (!contraseña) {
+//         Swal.showValidationMessage("No hay contraseña")
+//         return false;
+//     }
+//     let datos = baseDeDatos[usuario]
+//     if (!datos) {
+//         Swal.showValidationMessage("El usuario no existe")
+//         return false;
+//     }
+//     if (datos.contraseña! = contraseña) {
+//         Swal.showValidationMessage("Contraseña incorrecta")
+//         return false;
+//     }
+// }
+
+    // const checkBtn = document.querySelector('#check-btn');
+
+    // const messages = ['Usuario o contraseña incorrecta']
+
+    // function updateMessage(index) {
+    //     message.textContent = messages[index];
+    // }
+
+    // checkBtn.addEventListener('click', function () {
+    //     const response = input.value;
+    // })
